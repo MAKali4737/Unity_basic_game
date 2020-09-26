17923226385253c6e00cd723724414e90acac56d
MAKali4737 <38838164+MAKali4737@users.noreply.github.com>
2020-09-26 11:49:03 +0530

    Add files via upload

---
+++ Asset/Gamemanager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Gamemanager : MonoBehaviour
+{
+
+
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+  
+    public void EndGame ()
+    {
+
+        if(gameHasEnded == false )
+        {
+
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+
+
+    }
+
+    void Restart()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+
+}
